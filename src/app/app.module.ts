d05c1497748f3eb8eed18bd72896b3a168ab3595
athulya2000 <athulyav00@gmail.com>
2022-11-24 12:03:22 +0530

    delete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PatientEntry2Component } from './patient-entry2/patient-entry2.component';
 import { PatientSearchComponent } from './patient-search/patient-search.component';
+import { PatientDeleteComponent } from './patient-delete/patient-delete.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PatientEntry2Component,
-    PatientSearchComponent
+    PatientSearchComponent,
+    PatientDeleteComponent
   ],
   imports: [
     BrowserModule,

a4c079b1942919dd59fdd966b37a05f497e11998
athulya2000 <athulyav00@gmail.com>
2022-11-24 11:54:48 +0530

    search

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PatientEntry2Component } from './patient-entry2/patient-entry2.component';
+import { PatientSearchComponent } from './patient-search/patient-search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PatientEntryComponent,
-    Patiententry1Component,
-    PatientEntry2Component
+    PatientEntry2Component,
+    PatientSearchComponent
   ],
   imports: [
     BrowserModule,
